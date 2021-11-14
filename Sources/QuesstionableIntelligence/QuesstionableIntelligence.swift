e76ef66df22c575f6a8e2739642ad21df4eca3c7
Joseph Roque <contact@josephroque.ca>
2021-11-14 12:08:33 -0800

    Initial project commit

---
+++ Sources/QuesstionableIntelligence/QuesstionableIntelligence.swift
@@ -0,0 +1,10 @@
+//
+//  Created by Joseph Roque on 2021-11-14.
+//
+
+public struct QuesstionableIntelligence {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/QuesstionableIntelligenceTests/QuesstionableIntelligenceTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import QuesstionableIntelligence
+
+final class QuesstionableIntelligenceTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(QuesstionableIntelligence().text, "Hello, World!")
+    }
+}
